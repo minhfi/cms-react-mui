40e02f6899576a3bf498d10a80509eaf90878359
MPhi <minhfi.nguyen@gmail.com>
2023-06-06 13:16:03 +0700

    Config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 3000
+  },
   plugins: [
     react(),
     eslint(),
